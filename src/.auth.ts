9578b29c2901c18a0b626ec97d21889d997369d2
juzybits <96890594+juzybits@users.noreply.github.com>
2024-01-18 18:48:33 -0100

    move socialbots.ts to watch-trades.ts

---
+++ src/.auth.ts
@@ -0,0 +1,1 @@
+export const DISCORD_BOT_TOKEN = 'MTE5NzYyMTExMzU4NTQxMDA3OA.GdrG3y.4hS5ltAgPaN2hgP_Luux2EzVvdcpAmTqQAdHB4';
--- src/watch-trades.ts
+++ src/watch-trades.ts
@@ -1,12 +1,5 @@
-import { DiscordBot, TelegramBot, TwitterBot } from './bots.js';
 import { TurbosEventFetcher } from './TurbosEventFetcher.js';
 import { sleep } from './utils.js';
-
-console.log('Starting @polymedia/socialbots...');
-
-const twitterBot = new TwitterBot();
-const telegramBot = new TelegramBot();
-const discordBot = new DiscordBot();
 
 const FREQUENCY = 5_000;
 const EVENT_TYPE = '0x91bfbc386a41afcfd9b2533058d7e915a1d3829089cc268ff4333d54d6339ca1::pool::SwapEvent';
@@ -17,12 +10,13 @@ const eventFetcher = new TurbosEventFetcher(EVENT_TYPE, POOL_ID);
 async function main() {
     const events = await eventFetcher.fetchEvents();
     console.log(`(main) events.length: ${events.length}\n`);
-    // twitterBot.sendMsg(JSON.stringify(events));
-    // telegramBot.sendMsg(JSON.stringify(events));
-    // discordBot.sendMsg(JSON.stringify(events));
 }
 
 (async () => {
+    console.log('Starting watch-trades...');
+    console.log(`FREQUENCY: ${FREQUENCY}`);
+    console.log(`EVENT_TYPE: ${EVENT_TYPE}`);
+    console.log(`POOL_ID: ${POOL_ID}\n`);
     while (true) {
         await main();
         await sleep(FREQUENCY);
